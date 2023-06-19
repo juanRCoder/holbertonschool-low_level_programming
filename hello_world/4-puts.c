c7df4119acc72659c514e38a6e4a90fd0a926058
juanRCoder <guillecarck@gmail.com>
2023-06-19 11:58:50 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  *  main - Entry point of the program
- *   
+ *  
  *  Return: Always 0 (success)
- *    
  */
-int main(void) {
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }

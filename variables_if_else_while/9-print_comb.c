1b597d140b822a4a807ddfc6b506da710c5e427d
juanRCoder <guillecarck@gmail.com>
2023-06-20 14:15:37 -0700

    numbers with ','

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:This programming print array of base 10 numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

2a4df00b072746b6321c1d70d96d134645e83e6e
juanRCoder <guillecarck@gmail.com>
2023-06-20 15:05:02 -0700

    three numbers combinations

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,16 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 7; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
 			for (k = j + 1; k <= 9; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 				putchar((k % 10) + '0');
-				if (i != 8 || j != 9 || k != 7)
+				if (i != 7 || j != 8 || k != 9)
 				{
 					putchar(',');
 					putchar(' ');

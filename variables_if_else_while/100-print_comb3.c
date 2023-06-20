035c6f02a9f559ded514e05a23c8eddd91b8c956
juanRCoder <guillecarck@gmail.com>
2023-06-20 14:51:57 -0700

    numbers_combinations

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -12,10 +12,10 @@ int main(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = 1; j < 10; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
-			putchar('0' + i);
-			putchar('0' + j);
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
 			if (i != 8 || j != 9)
 			{
 				putchar(',');

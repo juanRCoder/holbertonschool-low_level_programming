f28483dabb826a0c9964f5eae43c9c6d61138380
juanRCoder <guillecarck@gmail.com>
2023-06-20 12:10:11 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Punto de entrada del programa
+ *
+ * Descripción: This function is print the alphabet.
+ *
+ * Return: Siempre devuelve 0 (éxito)
+ */
+
+int main (void)
+{
+	char *alphabet = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	return (0);
+}

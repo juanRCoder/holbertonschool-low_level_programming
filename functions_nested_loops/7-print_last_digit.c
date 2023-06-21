b5f802007fda94baeed39cd91f787e9c273e20ea
juanRCoder <guillecarck@gmail.com>
2023-06-21 14:21:35 -0700

    last_digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,17 @@
  * print_last_digit - Imprime el último dígito de un número
  * @n: El número del cual se imprimirá el último dígito
  *
- * Return: El último dígito de n
+ * Return: El último dígito de @n
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+
+	if (n < 0)
+		n = -n;
+
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }

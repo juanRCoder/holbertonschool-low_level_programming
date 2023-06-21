096b563c10e1390f7c1fa6d3810abdce1783f2b7
juanRCoder <guillecarck@gmail.com>
2023-06-21 13:59:07 -0700

    if else sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Imprime el signo de un número
+ * @n: El número a verificar
+ *
+ * Return: 1 si n es positivo, -1 si n es negativo, 0 si n es cero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

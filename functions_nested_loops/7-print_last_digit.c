42b1c311097d19148bd771b1f0843f6a46a97267
juanRCoder <guillecarck@gmail.com>
2023-06-21 14:15:02 -0700

    last_digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - Imprime el último dígito de un número
+ * @n: El número del cual se imprimirá el último dígito
+ *
+ * Return: El último dígito de n
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}

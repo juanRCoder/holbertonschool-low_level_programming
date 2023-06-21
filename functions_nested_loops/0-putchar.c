f0948ad63446ea7337bfc57965d8467cc493fb95
juanRCoder <guillecarck@gmail.com>
2023-06-21 12:31:55 -0700

    compiler _putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the word "_putchar" using the _putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char *word = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return (0);
+}

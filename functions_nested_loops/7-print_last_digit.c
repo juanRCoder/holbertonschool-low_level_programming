825f6921cb8cceac25b1d599884dae8ce346a34d
juanRCoder <guillecarck@gmail.com>
2023-06-21 14:49:12 -0700

    last_digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,12 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n == INT_MIN)
-	{
-		last_digit = 8;
-		_putchar(last_digit + '0');
-	}
-	else
-	{
-		if (n < 0)
-			n = -n;
-		last_digit = n % 10;
-		_putchar(last_digit + '0');
-	}
+	last_digit = n % 10;
+
+	if (n < 0)
+		last_digit = -last_digit;
+
+	_putchar(last_digit + '0');
+
 	return (last_digit);
 }

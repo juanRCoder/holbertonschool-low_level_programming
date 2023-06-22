5f434ad61911df82cea14737fc5382d2a006afbb
juanRCoder <guillecarck@gmail.com>
2023-06-22 07:32:42 -0700

    task9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -20,9 +20,9 @@ void times_table(void)
 				_putchar(' ');
 
 			if (result >= 10)
-				_putchar('0' + (product / 10));
+				_putchar('0' + (result / 10));
 
-			_putchar('0' + (product % 10));
+			_putchar('0' + (result % 10));
 		}
 		_putchar('\n');
 	}

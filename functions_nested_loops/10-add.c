f5fb511ef1c9dc8be3a56542f104472af42f2fb0
juanRCoder <guillecarck@gmail.com>
2023-06-21 17:37:25 -0700

    task10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - Adds two integers
+ * @num1: The first integer
+ * @num2: The second integer
+ *
+ * Return: The sum of the two integers
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+	return (sum);
+}

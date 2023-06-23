5d5ebeca59f9c36a0ed6c8c041f6c243d33f3244
juanRCoder <guillecarck@gmail.com>
2023-06-23 13:08:40 -0700

    task6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - Imprime una línea compuesta por el carácter '_'.
+ *
+ * @n: Número de veces que se debe imprimir el carácter '_'.
+ *
+ * Descripción: Esta función imprime una linea formada por '_'.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}

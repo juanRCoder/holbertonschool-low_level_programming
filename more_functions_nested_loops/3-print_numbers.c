9e50c2b7328f8dfa9cceda57824a69872bd428dc
juanRCoder <guillecarck@gmail.com>
2023-06-23 10:51:38 -0700

    task3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Imprime una serie de números en un rango específico.
+ *
+ * Descripción: Esta función se encarga de imprimir una secuencia de números
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+}
+

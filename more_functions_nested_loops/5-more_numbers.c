95de15db23ce5fd30718be232d930c91ed620f89
juanRCoder <guillecarck@gmail.com>
2023-06-23 12:21:58 -0700

    task5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,19 +2,19 @@
 /**
  * more_numbers - Imprime una serie extendida de números.
  *
- * Descripción: Esta función se encarga de imprimir una serie extendida 14 veces.
+ * Descripción: Esta función se encarga de imprimir una serie de 14 numbers..
  */
 void more_numbers(void)
 {
 	int i = 0, j;
 
-	while ( i < 15)
+	while (i < 15)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar('0' + j);	
+			_putchar('0' + j);
 
 		i++;
 	}
 	_putchar('\n');
-}	
+}

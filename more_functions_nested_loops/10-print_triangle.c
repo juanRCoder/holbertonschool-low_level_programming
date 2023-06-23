cc1736747788d161513d0b3122abe7f319638b9d
juanRCoder <guillecarck@gmail.com>
2023-06-23 15:13:37 -0700

    task10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,13 +15,13 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = 1; j < size - 1; j++)
+			for (j = 0; j < size - i - 1; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = size - i - 1; j <= size; j++)
+			for (j = size - i - 1; j < size; j++)
 			{
 				_putchar('#');
 			}

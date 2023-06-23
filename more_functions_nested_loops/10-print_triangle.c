3a72aa3268375689f7812bc02100a65e4a680ca3
juanRCoder <guillecarck@gmail.com>
2023-06-23 15:03:21 -0700

    task10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main"
+/**
+ * print_triangle - Imprime un triángulo formado por '#'.
+ * @size: Tamaño del triángulo.
+ *
+ * Descripción: Esta función imprime un triángulo de caracteres '#'.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j < size - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = size - i - 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

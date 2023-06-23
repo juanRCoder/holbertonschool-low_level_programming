caee960a638b2515ae13470b1444e1b08e7c3156
juanRCoder <guillecarck@gmail.com>
2023-06-23 11:04:39 -0700

    task4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to print specific numbers.
+ *
+ * Description: Print the numbers 0 through 9 minus the numbers 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+	}
+}

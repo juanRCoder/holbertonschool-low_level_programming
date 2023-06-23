9c85f0adc185e83eb444cbfb9618bb28cd456785
juanRCoder <guillecarck@gmail.com>
2023-06-23 10:33:52 -0700

    task2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Realiza la multiplicación de dos números enteros.
+ * @a: Primer número entero.
+ * @b: Segundo número entero.
+ *
+ * Return: El resultado de la multiplicación de @a por @b.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}

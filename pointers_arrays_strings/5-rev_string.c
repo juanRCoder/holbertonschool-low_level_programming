44f3c52e392fdf28b7aeb81ada3bc514f9f670bc
juanRCoder <guillecarck@gmail.com>
2023-06-26 14:41:01 -0700

    task5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rev_string - Reverses a string.
+ * @s: Pointer to the string to be reversed.
+ *
+ * Description: This function takes a pointer to a string and reverses the
+ *              order of its characters in place.
+ */
+void rev_string(char *s)
+{
+	char *start = s;
+	char *end = s;
+	char var;
+
+	while (*end)
+	{
+		end++;
+	}
+	end--;
+
+	while (start < end)
+	{
+		var = *start;
+		*start = *end;
+		*end = *var;
+
+		start++;
+		end--;
+	}
+}

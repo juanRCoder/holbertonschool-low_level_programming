245bbfa9468dbc194e197d630408d943c0bd7cc3
juanRCoder <guillecarck@gmail.com>
2023-06-26 10:16:29 -0700

    task1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - intercambia los valores de dos variables enteras
+ * @a: puntero a la primera variable
+ * @b: puntero a la segunda variable
+ *
+ * Descripción: Este programa modifica los valores a y b por otros.
+ */
+void swap_int(int *a, int *b)
+{
+	int var = *a; 
+	*a = *b;
+	*b = var;
+}

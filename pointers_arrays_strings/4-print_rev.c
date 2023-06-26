e1da5202854e8844774f2d52a4fd87310e2a4094
juanRCoder <guillecarck@gmail.com>
2023-06-26 13:23:18 -0700

    task4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - Imprime un string al revés.
+ * @s: Puntero al inicio del string.
+ *
+ * Return: No devuelve nada.
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	char *endC = s;
+
+	while (*endC != '\0')
+	{
+		length++;
+		endC++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

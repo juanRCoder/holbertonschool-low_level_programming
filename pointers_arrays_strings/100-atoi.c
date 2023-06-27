0aef5ed76e58f98efc49292f4d21ef13b618ed1e
juanRCoder <guillecarck@gmail.com>
2023-06-26 22:18:19 -0700

    task10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _atoi - Convierte una cadena en un número entero.
+ * @s: Puntero a la cadena de caracteres a convertir.
+ *
+ * Return: El número entero convertido.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int i = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[i] == '+')
+	{
+		i++;
+	}
+
+	while (s[i] > '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (sign * result);
+}

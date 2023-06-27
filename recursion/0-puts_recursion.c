f89f8cc27d595f3a75fc1539685be887316f528e
juanRCoder <guillecarck@gmail.com>
2023-06-27 10:48:23 -0700

    task0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - Imprime una cadena seguida de una nueva línea.
+ * @s: Puntero a la cadena a imprimir.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	_putchar(*s);
+
+	return (_puts_recursion(s + 1));
+}
---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+Aprendiendo la recursion con algunos ejercicios practicos!

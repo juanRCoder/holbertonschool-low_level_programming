493b412131360264c90d6cec8160d39b1563f4f4
juanRCoder <guillecarck@gmail.com>
2023-06-27 12:12:19 -0700

    task2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,11 +8,11 @@
 int _strlen_recursion(char *s)
 {
 	int length = 1;
-	
-	if(*s == '\0')
+
+	if (*s == '\0')
 	{
 		return (0);
 	}
 
-	return (length += strlen_recursion(s + 1));
+	return (length += _strlen_recursion(s + 1));
 }

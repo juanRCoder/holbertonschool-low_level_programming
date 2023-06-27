d96c137b28da926aca850bef093f21a1dad2d551
juanRCoder <guillecarck@gmail.com>
2023-06-26 20:34:04 -0700

    task7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * puts_half - Imprime la mitad de una cadena.
+ * @str: Puntero a la cadena.
+ *
+ * Descripción: Esta función imprime la mitad de una cadena, redondeando
+ * hacia abajo si la longitud de la cadena es impar.
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		length++;
+		ptr++;
+	}
+
+	if (length % 2 == 0)
+	{
+		ptr = str + (length / 2);
+	}
+	else
+	{
+		ptr = str + ((length - 1) / 2);
+	}
+
+	while (*ptr)
+	{
+		_putchar(*ptr);
+		ptr++;
+	}
+
+	_putchar('\n');
+}

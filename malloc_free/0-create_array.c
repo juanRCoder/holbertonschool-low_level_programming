3764cb9ea4b861c407ac0eae2f748a8e9b782a5b
juanRCoder <guillecarck@gmail.com>
2023-06-28 14:12:55 -0700

    task0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,22 +11,25 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *array = (char *)malloc(sizeof(char) * size);
+	*array = (char *)malloc(sizeof(char) * size);
 
 	if (array == NULL)
 	{
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
 
 	return (array);
-.}
+}

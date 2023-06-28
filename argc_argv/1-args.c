8757f5378b0a14aff7d1506214bd716c7070b033
juanRCoder <guillecarck@gmail.com>
2023-06-28 10:12:01 -0700

    task1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Punto de entrada del programa
+ * @argc: Número de argumentos pasados al programa
+ * @argv: Arreglo de cadenas de caracteres que contiene los argumentos
+ *
+ * Return: 0 en caso de éxito
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

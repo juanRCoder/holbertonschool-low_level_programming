faea6f77ac0672c3178aec5660c42301b2464436
juanRCoder <guillecarck@gmail.com>
2023-06-28 14:09:53 -0700

    task0

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - Creates an array of characters and initializes it with
+ * a specific character.
+ * @size: The size of the array to create.
+ * @c: The character to initialize each element of the array with.
+ *
+ * Return: On success, returns a pointer to the newly created array.
+ *         If 'size' is 0 or if the memory allocation fails, returns NULL.
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *array = (char *)malloc(sizeof(char) * size);
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+
+	return (array);
+.}
---
+++ malloc_free/README.md
@@ -0,0 +1,1 @@
+Estoy probando algunos ejercicos en MALLOC , en el mismo dia que ARGC and ARGV.

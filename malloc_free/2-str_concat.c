e4a95b0ecc452cbec8adad890f7bd64886621ce3
juanRCoder <guillecarck@gmail.com>
2023-06-30 14:34:23 -0700

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,6 +20,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	size_t len1, len2;
+	char *result;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -29,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
-	char *result = (char *)malloc(len1 + len2 + 1);
+	result = (char *)malloc(len1 + len2 + 1);
 
 	if (result == NULL)
 		return (NULL);

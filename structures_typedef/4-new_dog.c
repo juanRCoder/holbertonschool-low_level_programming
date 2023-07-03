9e8ed5873b887277498c2361f27105f612897970
juanRCoder <guillecarck@gmail.com>
2023-07-03 16:23:57 -0700

    task4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,6 +12,7 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int lenOw, lenName, i, j;
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
@@ -21,23 +22,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = strdup(name);
+	lenOw = strlen(owner);
+	lenName = strlen(name);
 
-	if (new_dog->name == NULL)
+	new_dog->name = malloc(lenName + 1);
+	new_dog->owner = malloc(lenOw + 1);
+
+	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
-		free(new_dog->name);
-		return (NULL);
-	}
-
-	new_dog->owner = strdup(owner);
-
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->owner);
 		free(new_dog);
+		free(new_dog->name);
+		free(new_dog->owner);
 		return (NULL);
 	}
-	new_dog->age = age;
 
+	for (i = 0; i < lenName + 1; i++)
+	{
+		new_dog->name[i] = name[i];
+	}
+
+	for (j = 0; j < lenOw + 1; j++)
+	{
+		new_dog->owner[j] = owner[j];
+	}
+
+	/* AGE */
+	new_dog->age = age;
 	return (new_dog);
 }

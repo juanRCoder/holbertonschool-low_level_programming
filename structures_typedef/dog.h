78e1be361f107fc6c706370d4f048a364523cd5e
juanRCoder <guillecarck@gmail.com>
2023-07-03 10:46:26 -0700

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Estructura de un perro.
+ *
+ * @name: Puntero al nombre del perro.
+ * @age: Edad del perro.
+ * @owner: Puntero al nombre del dueño.
+ *
+ * Description: Esta estructura representa un perro con su nombre,
+ * edad y el nombre del dueño.
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
